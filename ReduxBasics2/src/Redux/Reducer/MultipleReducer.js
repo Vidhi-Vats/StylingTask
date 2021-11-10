7d5f51720b160654b1457cd2354c710485135750
Vidhi Vats <vidhivats@TTNs-MacBook-Pro.local>
2021-11-10 13:15:52 +0530

    Mobxtask

--- ReduxBasics2/src/Redux/Reducer/MultipleReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux"
-import reducer from "./CountReducer";
-
-
-const  combinedReducers=combineReducers({
-    productval:reducer,
-})
-export default combinedReducers;
